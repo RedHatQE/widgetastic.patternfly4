e4b15b472ce9fd33e10ade7fa22980a30fe2e981
Dmitry Misharov <dmisharo@redhat.com>
2019-03-18 16:57:54 +0100

    Fixed dropdown tests

--- testing/test_dropdown.py
+++ testing/test_dropdown.py
@@ -7,7 +7,7 @@ from widgetastic_patternfly4 import Dropdown, DropdownItemDisabled, DropdownItem
 def view(browser):
     class TestView(View):
         dropdown = Dropdown("Dropdown")
-        kebab = Kebab(".//h1[normalize-space(.)='Kebab']/../"
+        kebab = Kebab(".//h3[normalize-space(.)='Kebab']/../"
                       "/div[contains(@class, 'pf-c-dropdown')]")
 
     return TestView(browser)
