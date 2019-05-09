ab08b288fedc0cc8f1bc78a1614608bfc9064d3b
Brandon Squizzato <bsquizza@redhat.com>
2019-05-09 11:02:34 -0400

    Skip flaky contextselector test in firefox

--- testing/test_contextselector.py
+++ testing/test_contextselector.py
@@ -34,6 +34,8 @@ def test_contextselector_open(view):
 
 
 def test_contextselector_item_select(view):
+    if view.browser.browser_type == "firefox":
+        pytest.skip("This test is flaky in headless firefox")
     view.contextselector.fill('AWS')
     assert view.contextselector.read() == 'AWS'
     assert not view.contextselector.is_open
