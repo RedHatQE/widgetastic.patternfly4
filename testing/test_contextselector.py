4ee65538cfb224b9399d99989368fcba9db81a75
Dmitry Misharov <dmisharo@redhat.com>
2019-06-26 13:28:52 +0200

    Changed scope of selenium fixture

--- testing/test_contextselector.py
+++ testing/test_contextselector.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 from widgetastic.widget import View
 from widgetastic_patternfly4 import ContextSelector, SelectItemNotFound
@@ -34,10 +33,7 @@ def test_contextselector_open(view):
     assert not view.contextselector.is_open
 
 
-@pytest.mark.skipif(os.environ["BROWSER"] == "firefox", reason="Failing in firefox headless")
 def test_contextselector_item_select(view):
-    if view.browser.browser_type == "firefox":
-        pytest.skip("This test is flaky in headless firefox")
     view.contextselector.fill('AWS')
     assert view.contextselector.read() == 'AWS'
     assert not view.contextselector.is_open
