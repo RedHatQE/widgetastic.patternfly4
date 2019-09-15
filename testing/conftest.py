04b6b03c8ca018634a4f0604268e3828449e91f7
Dmitry Misharov <quarckster@gmail.com>
2019-09-15 20:41:09 +0200

    Using docker in travis

--- testing/conftest.py
+++ testing/conftest.py
@@ -13,20 +13,9 @@ def browser_name():
 
 @pytest.fixture(scope="module")
 def selenium(browser_name):
-    if browser_name == "chrome":
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.headless = True
-        driver = webdriver.Chrome(options=chrome_options)
-    elif browser_name == "firefox":
-        firefox_options = webdriver.FirefoxOptions()
-        firefox_options.headless = True
-        driver = webdriver.Firefox(options=firefox_options)
-    elif browser_name == "remote_firefox":
+    if browser_name == "firefox":
         driver = webdriver.Remote(desired_capabilities=DesiredCapabilities.FIREFOX)
-    elif browser_name == "remote_chrome":
+    elif browser_name == "chrome":
         caps = DesiredCapabilities.CHROME.copy()
         caps["chromeOptions"] = {"args": ["disable-dev-shm-usage", "no-sandbox"]}
         driver = webdriver.Remote(desired_capabilities=caps)
