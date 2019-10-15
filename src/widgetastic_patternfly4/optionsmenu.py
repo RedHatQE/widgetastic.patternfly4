a9adfd040843be8cd7701dd3cbd30a7925b46c55
Brandon Squizzato <bsquizza@redhat.com>
2019-10-15 14:41:09 -0400

    Add the file

---
+++ src/widgetastic_patternfly4/optionsmenu.py
@@ -0,0 +1,18 @@
+from .dropdown import Dropdown
+
+
+class OptionsMenu(Dropdown):
+    BUTTON_LOCATOR = (
+        ".//button[contains(@class, 'pf-c-options-menu__toggle-button')]"
+    )
+    ITEMS_LOCATOR = ".//ul[contains(@class, 'pf-c-options-menu__menu')]/li"
+    ITEM_LOCATOR = (
+        ".//button[contains(@class, 'pf-c-options-menu__menu-item')"
+        " and normalize-space(.)={}]"
+    )
+    TEXT_LOCATOR = (
+        './/div[contains(@class, "pf-c-options-menu") and '
+        'descendant::span[contains(@class, "pf-c-options-menu__toggle-text") and '
+        'normalize-space(.)={}]]
+    )
+    DEFAULT_LOCATOR = './/div[contains(@class, "pf-c-options-menu")][1]'
