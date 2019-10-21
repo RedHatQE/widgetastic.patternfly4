520a2e9676cfd5a7db5a611630527c5eb37521d3
Dmitry Misharov <dmisharo@redhat.com>
2019-10-21 09:51:31 +0200

    Fixing url of components documentation

--- testing/conftest.py
+++ testing/conftest.py
@@ -27,7 +27,7 @@ def selenium(browser_name):
 def browser(selenium, request):
     name = request.module.__name__.split("_")[1]
     category = getattr(request.module, "CATEGORY", "components")
-    url = "https://patternfly-react.surge.sh/patternfly-4/{}/{}/?shadow=false"
+    url = "https://patternfly-react.surge.sh/patternfly-4/documentation/react/{}/{}"
     selenium.maximize_window()
     selenium.get(url.format(category, name))
     return Browser(selenium)
