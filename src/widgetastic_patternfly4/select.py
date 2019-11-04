7b7c2b4b6f2a5ac2313896432fc102912780bd43
Brandon Squizzato <bsquizza@redhat.com>
2019-11-04 16:06:24 -0500

    Add checkbox select

--- src/widgetastic_patternfly4/select.py
+++ src/widgetastic_patternfly4/select.py
@@ -1,19 +1,20 @@
 from contextlib import contextmanager
 
 from widgetastic.exceptions import NoSuchElementException
-from widgetastic.widget import GenericLocatorWidget
 from widgetastic.xpath import quote
 
+from .dropdown import Dropdown, DropdownItemNotFound, DropdownItemDisabled
 
-class SelectItemDisabled(Exception):
+
+class SelectItemDisabled(DropdownItemDisabled):
     pass
 
 
-class SelectItemNotFound(Exception):
+class SelectItemNotFound(DropdownItemNotFound):
     pass
 
 
-class Select(GenericLocatorWidget):
+class Select(Dropdown):
     """Represents the Patternfly Select.
 
     https://www.patternfly.org/v4/documentation/react/components/select
@@ -24,87 +25,21 @@ class Select(GenericLocatorWidget):
     ITEM_LOCATOR = (
         ".//button[contains(@class, 'pf-c-select__menu-item')" " and normalize-space(.)={}]"
     )
-
-    @contextmanager
-    def opened(self):
-        """A context manager to open and then close a Select."""
-        self.open()
-        yield
-        self.close()
-
-    @property
-    def is_open(self):
-        """Returns a boolean detailing if Select is open."""
-        return "pf-m-expanded" in self.browser.classes(self)
-
-    def open(self):
-        """Opens a Select."""
-        if not self.is_open:
-            self.browser.click(self.BUTTON_LOCATOR)
-
-    def close(self):
-        """Closes a Select."""
-        if self.is_open:
-            self.browser.click(self)
-
-    @property
-    def items(self):
-        """Returns a list of all Select items as strings."""
-        with self.opened():
-            result = [self.browser.text(el) for el in self.browser.elements(self.ITEMS_LOCATOR)]
-        return result
-
-    @property
-    def enabled_items(self):
-        """Returns a list of all enabled Select items as strings."""
-        result = []
-        with self.opened():
-            for el in self.browser.elements(self.ITEMS_LOCATOR):
-                item_value = self.browser.text(el)
-                if self.item_enabled(item_value):
-                    result.append(item_value)
-        return result
-
-    def has_item(self, item):
-        """Returns whether the items exists.
-
-        Args:
-            item: item name
-
-        Returns:
-            Boolean - True if enabled, False if not.
-        """
-        return item in self.items
+    TEXT_LOCATOR = (
+        './/div[contains(@class, "pf-c-select") and ' "child::button[normalize-space(.)={}]]"
+    )
+    DEFAULT_LOCATOR = './/div[contains(@class, "pf-c-select")][1]'
 
     def item_element(self, item, close=True):
         """Returns a WebElement for given item name."""
         try:
-            self.open()
-            result = self.browser.element(self.ITEM_LOCATOR.format(quote(item)))
-            if close:
-                self.close()
-            return result
-        except NoSuchElementException:
+            return super().item_element(item, close)
+        except DropdownItemNotFound:
             raise SelectItemNotFound(
                 "Item {!r} not found in {}. Available items: {}".format(
                     item, repr(self), self.items
                 )
             )
-
-    def item_enabled(self, item, close=True):
-        """Returns whether the given item is enabled.
-
-        Args:
-            item: Name of the item or item WebElement.
-
-        Returns:
-            Boolean - True if enabled, False if not.
-        """
-        el = self.item_element(item, close=False)
-        is_el_enabled = "pf-m-disabled" not in self.browser.classes(el)
-        if close:
-            self.close()
-        return is_el_enabled
 
     def item_select(self, item):
         """Opens the Select and selects the desired item.
@@ -115,16 +50,15 @@ class Select(GenericLocatorWidget):
         Raises:
             SelectItemDisabled: if item is disabled
         """
-        self.logger.info("Selecting %r in %r", item, self)
-        with self.opened():
-            if not self.item_enabled(item, close=False):
-                raise SelectItemDisabled(
-                    'Item "{}" of {} is disabled\n'
-                    "The following items are available and enabled: {}".format(
-                        item, repr(self), self.enabled_items
-                    )
+        try:
+            return super().item_select(item)
+        except DropdownItemDisabled:
+            raise SelectItemDisabled(
+                'Item "{}" of {} is disabled\n'
+                "The following items are available and enabled: {}".format(
+                    item, repr(self), self.enabled_items
                 )
-            self.browser.click(self.item_element(item, close=False))
+            )
 
     def fill(self, value):
         """Fills a Select with a value."""
@@ -134,5 +68,36 @@ class Select(GenericLocatorWidget):
         """Returns a string of the text of the selected option."""
         return self.browser.text(self.BUTTON_LOCATOR)
 
-    def __repr__(self):
-        return "{}({!r})".format(type(self).__name__, self.locator)
+
+class CheckboxSelect(Select):
+    ITEMS_LOCATOR = ".//label[contains(@class, 'pf-c-select__menu-item')]"
+    ITEM_LOCATOR = (
+        f"{ITEMS_LOCATOR}/span[starts-with(normalize-space(.), {{}})]/preceding-sibling::input"
+    )
+
+    def item_select(self, items):
+        if not hasattr(items, '__iter__'):
+            items = [items]
+        try:
+            for item in items:
+                element = self.item_element(item, close=False)
+                if not self.browser.is_selected(element):
+                    element.click()
+        finally:
+            self.close()
+
+    def item_deselect(self, items):
+        pass    
+
+    def fill(self, value):
+        """
+        {"item1": True, "item2": False}
+        """
+        pass
+
+    def read(self):
+        """
+        Returns {"item1": True, "item2": False}
+        """
+        pass
+    
