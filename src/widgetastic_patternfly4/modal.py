13cc6a8681a54afe4bac3b256ca50cd273ee5d60
john-dupuy <jdupuy@redhat.com>
2020-01-10 15:05:21 -0500

    Handle notifications when closing a modal

--- src/widgetastic_patternfly4/modal.py
+++ src/widgetastic_patternfly4/modal.py
@@ -22,6 +22,8 @@ class Modal(Widget):
     )
     TITLE = ".//h1[contains(@class, 'pf-c-title')]"
     CLOSE = ".//button[@aria-label='Close']"
+    NOTIFICATIONS = ".//div[contains(@class, 'pf-c-alert') and contains(@class, 'notification')]"
+    NOTIFICATION_ACTION = ".//div[contains(@class, 'pf-c-alert__action')]"
 
     def __init__(self, parent, locator=None, logger=None):
         super().__init__(parent, logger=logger)
@@ -37,8 +39,19 @@ class Modal(Widget):
         """Get WebElement of modal body."""
         return self.browser.element(self.BODY)
 
-    def close(self):
+    def close(self, close_notifications=True):
         """Close modal window."""
+        if close_notifications:
+            # close any alerts present
+            try:
+                notifications = self.root_browser.elements(self.NOTIFICATIONS)
+                for notification in notifications:
+                    action = self.root_browser.element(
+                        self.NOTIFICATION_ACTION, parent=notification
+                    )
+                    action.click()
+            except NoSuchElementException:
+                pass
         self.browser.click(self.browser.element(self.CLOSE))
 
     @property
