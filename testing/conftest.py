810fc02c8ece55c65ec35c5ed9462570049fa330
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2020-08-13 20:20:31 +0200

    IQE-593: steam line browser startup/selection
    * declared capabilities
    * --force-host to skip on running/re-running podman
    * use url instead of host in the fixture results
    * streamline waiting and browser creation
    * drop sudo

--- testing/conftest.py
+++ testing/conftest.py
@@ -5,9 +5,17 @@ from urllib.request import urlopen
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from wait_for import wait_for_decorator
+from wait_for import wait_for
 from widgetastic.browser import Browser
 
+
+CAPABILITIES = {
+    "firefox": DesiredCapabilities.FIREFOX,
+    "chrome": {
+        **DesiredCapabilities.CHROME,
+        "chromeOptions": {"args": ["disable-dev-shm-usage", "no-sandbox"]},
+    },
+}
 
 
 def pytest_addoption(parser):
@@ -17,54 +25,55 @@ def pytest_addoption(parser):
         choices=("firefox", "chrome"),
         default="firefox",
     )
+
+    parser.addoption("--force-host", default=None, help="foce a selenium hostname")
+
+
 @pytest.fixture(scope="session")
 def browser_name(pytestconfig):
     return os.environ.get("BROWSER") or pytestconfig.getoption("--browser-name")
 
 
 @pytest.fixture(scope="session")
-def selenium_host(worker_id):
-    oktet = 1 if worker_id == "master" else int(worker_id.lstrip("gw")) + 1
-    host = f"127.0.0.{oktet}"
-    ps = subprocess.run(
-        [
-            "sudo",
-            "podman",
-            "run",
-            "--rm",
-            "-d",
-            "-p",
-            f"{host}:4444:4444",
-            "-p",
-            f"{host}:5999:5999",
-            "--shm-size=2g",
-            "quay.io/redhatqe/selenium-standalone:latest",
-        ],
-        stdout=subprocess.PIPE,
-    )
-    yield host
-    container_id = ps.stdout.decode("utf-8").strip()
-    subprocess.run(["sudo", "podman", "kill", container_id])
+def selenium_url(pytestconfig, worker_id):
+    forced_host = pytestconfig.getoption("--force-host")
+
+    if forced_host is None:
+        oktet = 1 if worker_id == "master" else int(worker_id.lstrip("gw")) + 1
+        host = f"127.0.0.{oktet}"
+        ps = subprocess.run(
+            [
+                "podman",
+                "run",
+                "--rm",
+                "-d",
+                "-p",
+                f"{host}:4444:4444",
+                "-p",
+                f"{host}:5999:5999",
+                "--shm-size=2g",
+                "quay.io/redhatqe/selenium-standalone:latest",
+            ],
+            stdout=subprocess.PIPE,
+        )
+
+        yield f"http://{host}:4444/wd/hub"
+        container_id = ps.stdout.decode("utf-8").strip()
+        subprocess.run(["podman", "kill", container_id])
+    else:
+        yield f"http://{forced_host}:4444/wd/hub"
 
 
 @pytest.fixture(scope="session")
-def wait_for_selenium(selenium_host):
-    @wait_for_decorator(timeout=10, handle_exception=True)
-    def make_request():
-        urlopen(f"http://{selenium_host}:4444/wd/hub")
+def wait_for_selenium(selenium_url):
+    wait_for(lambda: urlopen(selenium_url), timeout=10, handle_exception=True)
 
 
 @pytest.fixture(scope="module")
-def selenium(browser_name, wait_for_selenium, selenium_host):
-    command_executor = f"http://{selenium_host}:4444/wd/hub"
-    if browser_name == "firefox":
-        driver = webdriver.Remote(
-            command_executor=command_executor, desired_capabilities=DesiredCapabilities.FIREFOX,
-        )
-    elif browser_name == "chrome":
-        caps = DesiredCapabilities.CHROME.copy()
-        caps["chromeOptions"] = {"args": ["disable-dev-shm-usage", "no-sandbox"]}
-        driver = webdriver.Remote(command_executor=command_executor, desired_capabilities=caps)
+def selenium(browser_name, wait_for_selenium, selenium_url):
+    driver = webdriver.Remote(
+        command_executor=selenium_url, desired_capabilities=CAPABILITIES[browser_name.lower()]
+    )
     yield driver
     driver.quit()
 
