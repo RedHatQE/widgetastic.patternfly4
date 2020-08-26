b5f5db8f0483bdae089c82a153bc3c0c6dde6c9a
Dmitry Misharov <dmisharo@redhat.com>
2020-08-26 11:43:58 +0200

    Added tests for OUIA Options Menu

---
+++ testing/ouia/test_optionsmenu.py
@@ -0,0 +1,45 @@
+import pytest
+from widgetastic.widget import View
+
+from widgetastic_patternfly4 import DropdownItemNotFound
+from widgetastic_patternfly4.ouia import OptionsMenuOUIA
+
+
+@pytest.fixture
+def options_menu(browser):
+    class TestView(View):
+        ROOT = ".//div[@id='ws-react-c-optionsmenu-ouia']"
+        options_menu = OptionsMenuOUIA("Simple Options Menu")
+
+    return TestView(browser).options_menu
+
+
+def test_options_menu_is_displayed(options_menu):
+    assert options_menu.is_displayed
+
+
+def test_enabled_options_menu(options_menu):
+    assert options_menu.is_enabled
+
+
+def test_options_menu_items(options_menu):
+    assert options_menu.items == ["Option 1", "Option 2", "Option 3"]
+    assert options_menu.has_item("Option 2")
+    assert not options_menu.has_item("Non existing items")
+    assert options_menu.item_enabled("Option 1")
+
+
+def test_options_menu_open(options_menu):
+    assert not options_menu.is_open
+    options_menu.open()
+    assert options_menu.is_open
+    options_menu.close()
+    assert not options_menu.is_open
+
+
+def test_options_menu_item_select(options_menu):
+    options_menu.item_select("Option 2")
+    assert options_menu.selected_items[0] == "Option 2"
+    assert not options_menu.is_open
+    with pytest.raises(DropdownItemNotFound):
+        options_menu.item_select("Non existing items")
