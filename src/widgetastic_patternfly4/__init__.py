b94c3e21ddeff41314b00fc65e805e61ec6e20a6
LightOfHeaven1994 <alexsham04.94@gmail.com>
2023-03-28 17:07:26 +0200

    Revert Add CalendarMonth widget

--- src/widgetastic_patternfly4/__init__.py
+++ src/widgetastic_patternfly4/__init__.py
@@ -2,7 +2,6 @@ from .alert import Alert
 from .breadcrumb import BreadCrumb
 from .bulletchart import BulletChart
 from .button import Button
-from .calendarmonth import CalendarMonth
 from .card import Card
 from .card import CardCheckBox
 from .card import CardForCardGroup
@@ -65,7 +64,6 @@ __all__ = [
     "BreadCrumb",
     "Button",
     "BulletChart",
-    "CalendarMonth",
     "CheckboxMenu",
     "CheckboxSelect",
     "Chip",
